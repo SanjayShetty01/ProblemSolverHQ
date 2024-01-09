f8196aacbd927c1ac7876649c256ffd05321c9b6
SanjayShetty01 <shettysanjay01@gmail.com>
2024-01-09 09:13:59 +0530

    Hamming distance

---
+++ Exercism_Solutiuon/R/Hamming.R
@@ -0,0 +1,25 @@
+uncover_string <- function(text){
+  return(unlist(strsplit(text, "")))
+}
+
+hamming <- function(strand1, strand2) {
+  stopifnot("Unequal Length" = nchar(strand1) == nchar(strand2))
+  strand1_split = uncover_string(strand1)
+  strand2_split = uncover_string(strand2)
+  sum(strand1_split == strand2_split)
+}
+
+
+
+###############################################################################
+
+a = "hi"
+b = "he"
+
+a_1 = uncover_string(a)
+b_1 = uncover_string(b)
+
+hamming(a_1, b_1)
+
+
+mapply(hamming, )
