2bff10acdcf2b806f86a6947566bcd20fbdb79d3
SanjayShetty01 <shettysanjay01@gmail.com>
2024-01-14 22:36:22 +0530

    sum_of_multiples

---
+++ Exercism_Solutiuon/R/Sum_of_Multiples.R
@@ -0,0 +1,36 @@
+get_multiples <- function(number, limit){
+  if (number < limit) {
+  multiples <- seq(number, limit - 1, by = number)
+  } else{
+    return(0)
+  }
+  return(multiples)
+}
+  
+sum_of_multiples <- function(factors, limit) {
+  multiples <- sapply(factors, get_multiples, limit = limit)
+  return(sum(unique(unlist(multiples))))
+}
+
+
+################################################################################
+
+factor = 3
+limit = 20
+
+seq(factor, limit - 1, by = factor) 
+
+g = seq(factor, limit - 1, by = factor) 
+
+sum(g) + q
+
+q = sum(g)
+
+get_multiples(c(3,5), 1)
+
+
+sapply(c(3,5), get_multiples, limit = 100) |>
+  unlist() |>
+  sum()
+
+sum_of_multiples(c(3,5), 100)
