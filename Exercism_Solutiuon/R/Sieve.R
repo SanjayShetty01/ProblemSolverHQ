be0409d178411581004876b696997bea16d9ce6e
SanjayShetty01 <shettysanjay01@gmail.com>
2024-01-15 18:58:06 +0530

    Sieve

--- Exercism_Solutiuon/R/Sieve.R
+++ Exercism_Solutiuon/R/Sieve.R
@@ -1,3 +1,31 @@
-sieve <- function(limit) {
-  
+is_prime <- function(x) {
+  if (x <= 1) return(FALSE)
+  if (x == 2) return(TRUE)
+  for (i in 2:sqrt(x)) {
+    if (x %% i == 0) return(FALSE)
+  }
+  return(TRUE)
 }
+
+get_primes_up_to_n <- function(n) {
+  if (n == 1) return(NULL)
+  primes <- c()
+  
+  for (i in 2:n) {
+    if (is_prime(i))  primes <- c(primes, i)
+  }
+  
+  return(primes)
+}
+
+sieve <- function(limit) {
+  get_primes_up_to_n(limit)
+}
+
+
+
+################################################################################
+
+is_prime(1)
+
+sieve(1)
