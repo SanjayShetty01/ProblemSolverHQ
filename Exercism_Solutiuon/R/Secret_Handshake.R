ec773134e7bac891bfa8e046054d594d39ada4a5
SanjayShetty01 <shettysanjay01@gmail.com>
2024-01-20 21:05:49 +0530

    secret handshake

--- Exercism_Solutiuon/R/Secret_Handshake.R
+++ Exercism_Solutiuon/R/Secret_Handshake.R
@@ -1,4 +1,13 @@
+
 handshake <- function(n) {
+  action <- c()
+  
+  if (bitwAnd(n, 1) > 0) action <- c(action, "wink")
+  if (bitwAnd(n, 2) > 0) action <- c(action, "double blink")
+  if (bitwAnd(n, 4) > 0) action <- c(action, "close your eyes")
+  if (bitwAnd(n, 8) > 0) action <- c(action, "jump")
+  
+  return(if (bitwAnd(n, 16) == 0) action else rev(action))
   
 }
 
@@ -9,3 +18,5 @@ handshake <- function(n) {
 intToBits(16)
 bitwAnd(15, 7)
 ?bitwAnd
+
+bitwAnd(1001, 16)
