e728de2b304688931efb26650bbbe2c1b5c0e955
SanjayShetty01 <shettysanjay01@gmail.com>
2024-01-21 18:59:51 +0530

    Luhn

--- Exercism_Solutiuon/R/Luhn_Algorithm.R
+++ Exercism_Solutiuon/R/Luhn_Algorithm.R
@@ -1,19 +1,19 @@
-# Determine whether the number is valid.
-luhn_algorithm <- function(number){
-  
-}
 
 is_valid <- function(input) {
   number <- input |> 
     gsub(" ", "", x = _) |> 
-    as.numeric() |> 
-    suppressWarnings() 
+    strsplit("") |>
+    unlist()
+
+  if (!all(number %in% as.character(0:9))) return(FALSE)
   
-  if (is.na(number) || (number == 0)) {
-    return(FALSE)
-  } else {
-    return(TRUE)
-  }
+  if (length(number) <= 1) return(FALSE)
+  
+  number <- as.numeric(number)
+  multip <- rep(c(1,2), length.out = length(number)) |> rev()
+  number <- number * multip
+  number <- ifelse(number > 9, number - 9, number)
+  (sum(number) %% 10) == 0
 }
 
 
@@ -31,3 +31,4 @@ number <- input |>
 
 is.na(number)
 
+is_valid("0")
