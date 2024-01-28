8f63767dc41a3d0007cdbcc7d8b6499796546501
SanjayShetty01 <shettysanjay01@gmail.com>
2024-01-28 17:04:39 +0530

    tournament

--- Exercism_Solutiuon/R/Tournament.R
+++ Exercism_Solutiuon/R/Tournament.R
@@ -1,6 +1,48 @@
-tournament <- function(input) {
+tournament <- function (input) {
+  input <- input[grepl("^[a-zA-Z ]*;[a-zA-Z ]*;(win|loss|draw)$", input)]
   
+  
+  data = read.table(text = input, sep = ";", header = F, 
+                    col.names = c("Team_1", "Team_2", "Result"))
+  
+  teams <- unique(c(data$Team_1, data$Team_2)) |> sort()
+  
+  n <- length(teams)    
+  MP <- numeric(n); names(MP) <- teams
+  W  <- numeric(n); names(W)  <- teams
+  L  <- numeric(n); names(L)  <- teams
+  D  <- numeric(n); names(D)  <- teams
+  
+  for (i in 1:nrow(data)) {
+    Team_1 <- data$Team_1[i]
+    Team_2 <- data$Team_2[i]
+    
+    Result <- data$Result[i]
+    
+    MP[Team_1] <- MP[Team_1] + 1
+    MP[Team_2] <- MP[Team_2] + 1
+    
+    if (Result == "win") {
+      W[Team_1] <- W[Team_1] + 1
+      L[Team_2] <- L[Team_2] + 1
+    } else if (Result == "loss") {
+      L[Team_1] <- L[Team_1] + 1
+      W[Team_2] <- W[Team_2] + 1
+    } else {
+      D[Team_1] <- D[Team_1] + 1
+      D[Team_2] <- D[Team_2] + 1
+    }
+  }
+  
+  P <- W * 3 + D
+  
+  score_card <- data.frame(Team = teams, MP = MP, W = W, D = D, L = L, P = P)
+  score_card <- score_card[order(-P, teams),]
+  row.names(score_card) <- 1:nrow(score_card)
+  score_card
 }
+
+
 
 
 
@@ -18,8 +60,53 @@ input <- c(
 )
 
 
-g = read.table(text = input, sep = ";", header = F, 
+data = read.table(text = input, sep = ";", header = F, 
                col.names = c("Team_1", "Team_2", "Result"))
+
+teams <- unique(c(data$Team_1, data$Team_2)) |> sort()
+
+n <- length(teams)    
+MP <- numeric(n); names(MP) <- teams
+W  <- numeric(n); names(W)  <- teams
+L  <- numeric(n); names(L)  <- teams
+D  <- numeric(n); names(D)  <- teams
+
+for (i in 1:nrow(data)) {
+  Team_1 <- data$Team_1[i]
+  Team_2 <- data$Team_2[i]
+  
+  Result <- data$Result[i]
+  
+  MP[Team_1] <- MP[Team_1] + 1
+  MP[Team_2] <- MP[Team_2] + 1
+  
+  if (Result == "win") {
+    W[Team_1] <- W[Team_1] + 1
+    L[Team_2] <- L[Team_2] + 1
+  } else if (Result == "loss") {
+      L[Team_1] <- L[Team_1] + 1
+      W[Team_2] <- W[Team_2] + 1
+    } else {
+      D[Team_1] <- D[Team_1] + 1
+      D[Team_2] <- D[Team_2] + 1
+    }
+}
+
+P <- W * 3 + D
+
+score_card <- data.frame(Team = teams, MP = MP, W = W, D = D, L = L, P = P)
+score_card <- score_card[order(-P, teams),]
+row.names(score_card) <- 1:nrow(score_card)
+score_card
+
+
+
+
+score_card <- data.frame(matrix(nrow = length(teams), 
+                                ncol = 6))
+
+colnames(score_card) <- c("Team", "MP", "W", "D", "L", "P")
+
 g
 
 # g$Winner <- ifelse(g$Result == "win", g$Team_1, g$Team_2)
@@ -49,7 +136,7 @@ g$Drew <- ifelse(g$Result == "draw", T, F)
 
 g
 
-names <- unique(c(g$Team_1, g$Team_2))
+names <- unique(c(g$Team_1, g$Team_2)) |> sort()
 
 a = table(c(g$Team_1, g$Team_2))
 
